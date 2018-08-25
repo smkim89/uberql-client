ad3fe7adff8f009e759d9b9eefbe3bf948750d37
smkim89 <kiak357@gmail.com>
2018-08-26 01:34:32 +0900

    Header에 jwt 설정.

--- src/Apollo.ts
+++ src/Apollo.ts
@@ -1,6 +1,15 @@
-import ApolloClient from "apollo-boost";
+import ApolloClient, { Operation } from "apollo-boost";
+
 
 const client = new ApolloClient({
+    // apollo 에서는 모든 리퀘스트에 아래와같이 데이터를 심을 수 있다. 리덕트에서는 설정을해야함.
+    request: async (operation: Operation) => {
+        operation.setContext({
+          headers: {
+            "X-JWT": localStorage.getItem("jwt") || ""
+          }
+        });
+      },
     uri : "http://localhost:4000/graphql"
 });
 
